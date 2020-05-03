d8c4d760e134eda0074dbaa9b6f3c59fb6c6f553
Akash <akasharul2101@gmail.com>
2020-05-03 19:25:56 +0530

    removed mask images concept and added mask as an alpha channel

--- dg.py
+++ dg.py
@@ -19,13 +19,8 @@ class DataSetGenerator:
 		"""
 		self.name = name
 		self.ids = []
-		self.paths = []
+		self.img_paths = []
 		self.bgs = []
-
-	def __getitem__(self,idx):
-		return {'image_id': self.ids[idx],
-				'path':self.paths[idx],
-				'bg':self.bgs[idx]}
 		
 	def generate(self):
 		"""call general functions for data generation"""
@@ -48,13 +43,13 @@ class DataSetGenerator:
 		for _ in range(self.count):
 			id_ = random.randint(10000,99999)
 			self.ids.append(id_)
-			self.paths.append(
-				f"{self.name}/images/{id_}.jpg")
+			self.img_paths.append(
+				f"{self.name}/images/{id_}.png")
 
 	def create_json(self):
 		"""create json file to store ids,paths and bgs"""
 		data = {'image_id':self.ids,
-				'path':self.paths,
+				'img_path':self.img_paths,
 				'bg':self.bgs}
 		if hasattr(self,'bbox'):
 			data['bbox'] = self.bbox
@@ -88,10 +83,10 @@ class PlainSet(DataSetGenerator):
 	def gen(self):
 			"""generate images and return path ,img"""
 			c = self.channels
-			for path,bg in zip(self.paths,self.bgs):
+			for path,bg in zip(self.img_paths,self.bgs):
 				plain = np.ones((self.h,self.w,c),
 								dtype=np.uint8)
-				yield path,plain*bg
+				yield path,(plain*bg).astype(np.uint8)
 
 	def create_list(self):
 		"""
@@ -123,17 +118,6 @@ class ObjectSet(DataSetGenerator):
 		self.object = obj
 		self.bbox = []
 
-
-class ObjectOverPlainSet(ObjectSet,PlainSet):
-	"""object over plain images ."""
-	#so inherited both objectset and plainset .  
-	def __init__(self,name,obj,bg=None):
-		"""
-		get the object as well as colour of 
-		the plain image if given
-		"""
-		super().__init__(name,obj,bg=bg)
-
 	def create_list(self):
 		"""
 		add bbox attribute to the list 
@@ -145,12 +129,33 @@ class ObjectOverPlainSet(ObjectSet,PlainSet):
 			y = random.randrange(0,self.h-h)
 			self.bbox.append([x,y,w,h])
 
+
+class ObjectOverPlainSet(ObjectSet,PlainSet):
+	"""object over plain images ."""
+	#so inherited both objectset and plainset .  
+	def __init__(self,name,obj,bg=None):
+		"""
+		get the object as well as colour of 
+		the plain image if given
+		"""
+		super().__init__(name,obj,bg=bg)
+
 	def gen(self):
-		for (x,y,w,h),(path,plain) in zip(self.bbox,super().gen()):
+		for (x,y,w,h),(path,plain) in zip(self.bbox,
+									super().gen()):
 			plain[x:x+w,y:y+h] = self.object
+			plain = cv2.cvtColor(plain,cv2.COLOR_RGB2RGBA)
+			mask = np.zeros((self.h,self.w),
+							dtype=np.uint8)
+			mask[x:x+w,y:y+h] = 255
+			plain[:,:,3] = mask
 			yield path,plain
 
 
-ob = ObjectOverPlainSet("dataset",'skystone.png')
+ob = ObjectOverPlainSet("dataset",'skystone.png',
+						[(0,0,0),(255,255,255)])
 ob.cleanup()
 ob.generate((1000,1000),10)
+
+img =  cv2.imread(ob.img_paths[0],-1)
+print(img.shape)
